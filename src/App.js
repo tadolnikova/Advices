00f84706c8b98124ecdad88f3cf15e1d03448097
Татьяна <tatyana@iMac-Tatana.local>
2022-11-20 16:42:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import image from './11.webp';
+
+
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+useEffect(() => {
+getAdvice();
+}, [])
+
+
+  const getAdvice = async () =>{
+    const response = await fetch (`https://www.boredapi.com/api/activity/ `)
+    const data = await response.json()
+    setAdvice(data.activity)
+}
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Are you bored?</h1>
+      <img src={image}  alt="tips" width="400px" />
+    <h2>{advice}</h2>
+    <button onClick={getAdvice}>Сlick and find out what to do</button>
+    
     </div>
   );
 }
